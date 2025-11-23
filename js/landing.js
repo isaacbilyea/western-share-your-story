eee594c15f5006d8e47c77188e56bede089a9a7f
Isaac Bilyea <isaacbilyea@gmail.com>
2025-11-23 10:12:12 -0500

    Add carousel component and update landing page styles
    Introduces a new carousel module and associated SCSS for featured stories, updates landing page HTML and JS to integrate carousel functionality, and adds SVG assets for carousel navigation. Refines story card and carousel styles for improved responsiveness and user experience.

--- js/landing.js
+++ js/landing.js
@@ -1,5 +1,10 @@
 import { mobileMenu } from './modules/mobile-menu.js';
 import { footerAccordion } from './modules/footer-accordion.js';
+import { carousel } from './modules/carousel.js';
 
 mobileMenu();
 footerAccordion();
+
+carousel('.story-stat-groups');
+carousel('.story-cards');
+carousel('.why-share-items');
